bdf20e1509df2f58d9dcd92ca000addcaead15a4
Taoufik <aslimani.taoufik@gmail.com>
2024-08-08 17:46:47 +0200

    Ajout nouveau Backend nettoyé

---
+++ SchoolManagementApi/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SchoolManagementApi.Data;
+using SchoolManagementApi.Models;
+using System.Threading.Tasks;
+
+namespace SchoolManagementApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly SchoolContext _context;
+
+        public AuthController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(User login)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == login.Username && u.Password == login.Password);
+            if (user == null)
+                return Unauthorized();
+            return Ok(user);
+        }
+    }
+}
---
+++ SchoolManagementApi/Controllers/StudentEnrollment.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SchoolManagementApi.Data;
+using SchoolManagementApi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SchoolManagementApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentEnrollmentsController : ControllerBase
+    {
+        private readonly SchoolContext _context;
+
+        public StudentEnrollmentsController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<StudentEnrollment>>> GetEnrollments()
+        {
+            return await _context.StudentEnrollments.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<StudentEnrollment>> EnrollStudent(StudentEnrollment enrollment)
+        {
+            _context.StudentEnrollments.Add(enrollment);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetEnrollments), new { id = enrollment.Id }, enrollment);
+        }
+    }
+}
