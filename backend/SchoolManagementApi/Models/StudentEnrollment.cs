75d7f9dbf4208d5442aadd30cb5d57dd30c8cf8b
Taoufik <aslimani.taoufik@gmail.com>
2024-08-20 14:26:57 +0200

    Supprimer le répertoiretemp backend

--- backend/SchoolManagementApi/Models/StudentEnrollment.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SchoolManagementApi.Models
-{
-    public class StudentEnrollment
-    {
-        public int Id { get; set; }
-        public int StudentId { get; set; }
-        public int CourseId { get; set; }
-    }
-}
